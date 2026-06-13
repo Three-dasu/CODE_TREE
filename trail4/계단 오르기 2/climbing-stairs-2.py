30e6f2a1ee2e473aed1cbae92eb931a96dbf5f48
Three-dasu <gyuul173@gmail.com>
2026-06-13 15:05:00 +0900

    [Passed] 계단 오르기 2 (48ms, 16MB)

--- trail4/계단 오르기 2/climbing-stairs-2.py
+++ trail4/계단 오르기 2/climbing-stairs-2.py
@@ -1,23 +1,27 @@
 N = int(input())
-lst = [0] + list(map(int, input().split()))
+coin = [0] + list(map(int, input().split()))
 
 # dp[i][j]: i번째 계단까지 j번의 1계단 점프로 갔을 때의 최대 동전 개수
 dp = [[-1]*(4) for _ in range(N+1)]
 dp[0][0] = 0
-dp[1][1] = lst[1]
+dp[1][1] = coin[1]
 
-for i in range(N):
+for i in range(2, N+1):
     for j in range(4):
-        if dp[i][j] != -1:
-            # j가 3인 경우 -> 1계단 점프 불가 -> 기존 vs 2계단 점프
-            if j == 3 and i <= N-2:
-                dp[i+2][j] = max(dp[i+2][j], dp[i][j] + lst[i+2])
+        
+        # j가 0인 경우 -> 무조건 2계단 점프
+        if j == 0 and dp[i-2][j] != -1:
+            dp[i][j] = max(dp[i][j], dp[i-2][j] + coin[i])
 
-            # 기존 vs 1계단 점프
-            if j <= 2:
-                dp[i+1][j+1] = max(dp[i+1][j+1], dp[i][j] + lst[i+1])
+        # 기존 vs 1계단 점프 vs 2계단 점프
+        if 1 <= j <= 3:
+            if dp[i-2][j] != -1:
+                dp[i][j] = max(dp[i][j], dp[i-2][j] + coin[i])
 
-            if i <= N-2:
-                dp[i+2][j] = max(dp[i+2][j], dp[i][j] + lst[i+2])
+            if dp[i-1][j-1] != -1:
+                dp[i][j] = max(dp[i][j], dp[i-1][j-1] + coin[i])
+
+            # if dp[i-1][j-1] != -1 and dp[i-2][j] != -1:
+            #     dp[i][j] = max(dp[i][j], dp[i-1][j-1] + coin[i], dp[i-2][j] + coin[i])
 
 print(max(dp[N]))
