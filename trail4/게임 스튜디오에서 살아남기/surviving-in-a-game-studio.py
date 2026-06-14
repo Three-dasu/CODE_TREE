4a9e49e246e6591f0cbea309a03863083a85bfef
Three-dasu <gyuul173@gmail.com>
2026-06-14 11:39:06 +0900

    [Passed] 게임 스튜디오에서 살아남기 (89ms, 18MB)

--- trail4/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.py
+++ trail4/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.py
@@ -4,30 +4,27 @@ N = int(input())
 # dp[i][j][k]: i번째 날에 받은 j개의 B와 k개의 T로 살아남은 경우의 수
 # 이진수로 한다면...?! 차이가 없는데 말만 2차원이지 배열 요소 개수는 똑같
 
-dp = [[[0]*3 for _ in range(3)] for _ in range(N+1)]
-dp[0][0][0] = 1
+dp = [[0]*3 for _ in range(3)]
+dp[0][0] = 1
 
 for i in range(N):
+    ndp = [[0]*3 for _ in range(3)]
 
     for j in range(3):
         for k in range(3):
 
             # G: B 개수 초기화
-            dp[i+1][0][k] += dp[i][j][k]
+            ndp[0][k] += dp[j][k] % (10**9 + 7)
 
             # B
             if j < 2:
-                dp[i+1][j+1][k] += dp[i][j][k]
+                ndp[j+1][k] += dp[j][k] % (10**9 + 7)
 
             # T: B 개수 초기화
             if k < 2:
-                dp[i+1][0][k+1] += dp[i][j][k]
+                ndp[0][k+1] += dp[j][k] % (10**9 + 7)
+
+    dp = ndp
 
 
-    # for row in dp:
-    #     for val in row:
-    #         print(val, end=' ')
-    #     print()
-    # print()
-
-print(sum(sum(row) for row in dp[N]) % (10**9 + 7))
+print(sum(sum(row) for row in dp) % (10**9 + 7))
