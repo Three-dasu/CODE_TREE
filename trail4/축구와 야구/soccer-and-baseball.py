8b4f395b6ac8403d7736343d799fb2801292a101
Three-dasu <gyuul173@gmail.com>
2026-06-15 14:14:52 +0900

    [Passed] 축구와 야구 (121ms, 20MB)

--- trail4/축구와 야구/soccer-and-baseball.py
+++ trail4/축구와 야구/soccer-and-baseball.py
@@ -6,46 +6,40 @@ for _ in range(N):
     s.append(si)
     b.append(bi)
 
-def myp():
-    for row in dp:
-        for val in row:
-            print(val, end=' ')
-        print()
-    print()
-
 # 구하는 것: 20명 능력 합의 최대
 # 요소: 고려한 사람, 축구합, 야구합, 총합
 # dp[i][j][k] = 사람 i명 고려했을 떄, 축구 j, 야구 k명 채운 상황에서 능력치 합의 최댓값
-# dp[j][k] = 굳이 사람 i명 따질 필요 없을 듯. 축구 j, 야구 k명 채운 상황에서 능력치 합의 최댓값
-J, K = 11, 9    # 채워야 하는 사람 수
+dp = [[[0]*(10) for _ in range(12)] for _ in range(N+1)]
+dp[0][0][0] = 0
 
-dp = [[-1]*(K+1) for _ in range(J+1)]
-dp[0][0] = 0
 
+"""
+근데 새로운 사람을 고려할 때 20명이 다 찬 상태면 기존 인원을 빼고 넣어야 하잖아
+그럼 그냥 한명씩 보면서 하면 되는 거 아닌가
+1000x20
+"""
 
 for i in range(N):
-    for j in range(J, -1, -1):
-        for k in range(K, -1, -1):
-            # if j + k > i:
-            #     continue
-            # if dp[j][k] == -1:
-            #     continue
+    for j in range(12):
+        for k in range(10):
+            if j + k > i:
+                continue
 
-            # # 축구 인원 여유 축구에 배정
-            # if j < J:
-            #     dp[j][k] = max(dp[j][k], dp[j-1][k] + s[i])
+            # 안넣는 경우
+            dp[i+1][j][k] = max(dp[i+1][j][k], dp[i][j][k])
 
-            # 기존 축구 한명 빼고 축구 새 사람 추가
-            if j > 0 and dp[j-1][k] != -1:
-                dp[j][k] = max(dp[j][k], dp[j-1][k] + s[i])
-
-            # # 야구 인원 여유 야구에 배정
-            # if k < K:
-            #     dp[j][k+1] = max(dp[j][k+1], dp[j][k] + b[i])
+            if j < 11:
+                dp[i+1][j+1][k] = max(dp[i+1][j+1][k], dp[i][j][k] + s[i])
             
-            # 기존 야구 한명 빼고 새 사람 추가
-            if k > 0 and dp[j][k-1] != -1:
-                dp[j][k] = max(dp[j][k], dp[j][k-1] + b[i])
+            if k < 9:
+                dp[i+1][j][k+1] = max(dp[i+1][j][k+1], dp[i][j][k] + b[i])
 
-    # myp(1)
-print(dp[J][K])
+# def myp(n):
+#     for row in dp[n]:
+#         for val in row:
+#             print(val, end=' ')
+#         print()
+#     print()
+
+# myp(1)
+print(dp[N][11][9])
