7cbecf22704edc43ec74a8e43c05222d0e4b9de0
Three-dasu <gyuul173@gmail.com>
2026-06-24 10:34:42 +0900

    [Passed] 최대 합 분할 (117ms, 24MB)

--- trail4/최대 합 분할/maximum-sum-partition.py
+++ trail4/최대 합 분할/maximum-sum-partition.py
@@ -1,26 +1,46 @@
 N = int(input())
 lst = list(map(int, input().split()))
 
+"""
+A합과 B합이 같게, 대신 그 합이 최대가 되게끔
+
+A와 B의 차이가 같고, A합이 같으면 같은 상황이지 않은가
+그럼 A와 B의 차이에서 A합을 최대화 하는 걸로 하면
+dp[0]하면 되는 거 아닐까
+
+dp[i] = A와 B의 차이 i 에서 무거운 쪽의 합 최댓값
+"""
+
 tot = sum(lst)
-# dp[i]: 차이가 i인 조합의 큰 부분 합
-dp = [-1] * (tot + 1)
+dp = [-1] * (tot+1)
 dp[0] = 0
 
-for n, num in enumerate(lst):
+for w in lst:
     ndp = dp[:]
     for i in range(tot, -1, -1):
-        if dp[i] != -1:
-            
-            # 큰 쪽 = dp[i]
-            # 작은 쪽 = dp[i] - i
+        if dp[i] == -1:
+            continue
+        
+        # 무거운 쪽에 넣기
+        nw = i + w
+        if nw <= tot:
+            ndp[nw] = max(ndp[nw], dp[i] + w)
+        
+        # 가벼운 쪽에 넣기
+        cw = dp[i] - i  # 현재 무게 = 무거운 쪽 - 차이
+        nw = cw + w
 
-            # 작은 쪽에 더하기
-            new_diff = abs(i - num)
-            ndp[new_diff] = max(ndp[new_diff], dp[i]-i + num, dp[i])
+        # 가벼운 쪽에 넣었지만 여전히 가볍다면
+        if nw < dp[i]:
+            ndp[dp[i]-nw] = max(ndp[dp[i]-nw], dp[i])
+        # 가벼운 쪽에 넣어서 무거워졌다면
+        else:
+            ndp[nw-dp[i]] = max(ndp[nw-dp[i]], nw)
 
-            # 큰 쪽에 더하기
-            new_diff = i + num
-            ndp[new_diff] = max(ndp[new_diff], dp[i] + num)
+        # # 버리기
+        # nw = i
+        # dp[i] = max(dp[i], dp[i] + w)
+    # print(ndp)
     dp = ndp
 
 print(dp[0])
