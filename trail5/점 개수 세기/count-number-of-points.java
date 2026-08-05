4c489d1462612bbc42eb7e7abb9cd4eb9b5ea477
cothe-baek <gyuul173@gmail.com>
2026-08-05 11:31:35 +0900

    [Passed] 점 개수 세기 (1658ms, 39MB)

--- trail5/점 개수 세기/count-number-of-points.java
+++ trail5/점 개수 세기/count-number-of-points.java
@@ -7,7 +7,7 @@ public class Main {
     static Set<Integer> tSet = new TreeSet<>();
     
     static int bisectLeft(int[] lst, int x) {
-        int left = 0;int right = lst.length;
+        int left = 0; int right = lst.length;
         int mid;
 
         while (left < right) {
@@ -54,14 +54,16 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        Scanner sc = new Scanner(System.in);
+        N = Integer.parseInt(st.nextToken());
+        Q = Integer.parseInt(st.nextToken());
 
-        N = sc.nextInt(); Q = sc.nextInt();
-
+        st = new StringTokenizer(br.readLine());
         point = new int[N];
         for (int i=0; i<N; i++) {
-            point[i] = sc.nextInt();
+            point[i] = Integer.parseInt(st.nextToken());
         }
 
         compress();
@@ -70,8 +72,9 @@ public class Main {
 
 
         for (int q=0; q<Q; q++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
             // q[0]값을 bisectLeft로 point에서 찾아야 함?
             int li = bisectLeft(lst, a);
